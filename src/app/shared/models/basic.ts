e3d40d3eebf49f27ccbb405badf68a4fab50329f
呉翔宇 <sangwoooh1013@gmail.com>
2020-11-27 22:53:03 +0900

    ラジオボタンダミーデータ入力

---
+++ src/app/shared/models/basic.ts
@@ -0,0 +1,13 @@
+export class Basic {
+  id: number;
+  label01: string;
+  label02: string;
+  label03: string;
+
+  constructor(id: number, label01: string, label02: string, label03: string) {
+    this.id = id;
+    this.label01 = label01;
+    this.label02 = label02;
+    this.label03 = label03;
+  }
+}
--- src/app/test/basic/basic.component.ts
+++ src/app/test/basic/basic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Basic } from '../../shared/models/basic';
 
 @Component({
   selector: 'app-basic',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basic.component.css']
 })
 export class BasicComponent implements OnInit {
+  basics: Basic[] = [
+    new Basic(1, '男性', '女性', 'その他')
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.basics = [
+      new Basic(1, '男性', '女性' ,'その他'),
+      new Basic(2, '男性', '女性' ,'その他'),
+      new Basic(3, '男性', '女性' ,'その他'),
+      new Basic(4, '男性', '女性' ,'その他'),
+      new Basic(5, '男性', '女性' ,'その他'),
+    ];
   }
 
 }
